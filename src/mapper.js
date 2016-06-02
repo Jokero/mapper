3343bc6e0089f08f7c0f127fe202b07418d18dce
tamtakoe <tamtakoe@gmail.com>
2016-06-02 12:15:37 +0300

    v0.2.1

---
+++ src/mapper.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const map = require('./map');
+
+/**
+ * @param {Object|Object[]} schemaOrData
+ * @param {Object}          [schema]
+ *
+ * @returns {Function|Object|Object[]}
+ */
+module.exports = function(schemaOrData, schema) {
+    if (!schema) {
+        return function(value) {
+            return map(value, schemaOrData);
+        };
+    }
+
+    return map(schemaOrData, schema);
+};
